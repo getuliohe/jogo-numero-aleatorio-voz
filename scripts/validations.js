9a75c123bc32220bf30870a0b5908c4ad787ce50
Getulio Lima <getuliofurtad03@gmail.com>
2024-04-16 09:30:17 -0300

    adicionando game over

--- scripts/validations.js
+++ scripts/validations.js
@@ -1,22 +1,38 @@
 function verifyIfValidValue(guess){
+    if(guess==='game over'){
+        document.body.innerHTML = `
+        <div class="container">
+            <h1 class="title">Você desistiu ;-;!!</h1>
+            <h3 class="subtitle">o número secreto era ${randomNumber}</span></h3>
+
+            <button id="restart">Jogar Novamente</button>
+        </div>
+        `
+        return
+    }
+    
     const number = +guess
 
     if (invalidGuess(number)) {
         guessElement.innerHTML += `
         <div>Inválido!! Não há somente números</div>
         `
+        return
     }else if(numberHigherOrLowerThanAlowed(number)){
         guessElement.innerHTML += `
         <div>O número está fora da adivinhação, precisa estar entre ${minNumber} e ${maxNumber}</div>
         `
+        return
     }else if(numberHigherThanRandom(number)){
         guessElement.innerHTML += `
         <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
         `
+        return
     }else if(numberLowerThanRandom(number)){
         guessElement.innerHTML += `
         <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
         `
+        return
     }else if(number === randomNumber){
         document.body.innerHTML = `
         <div class="container">
@@ -26,6 +42,7 @@ function verifyIfValidValue(guess){
             <button id="restart">Jogar Novamente</button>
         </div>
         `
+        return
     }
 }
 
